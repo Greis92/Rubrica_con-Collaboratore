31d3c2e5d57f6b4a6ee76d43b107baf7184a46e2
Greis92 <graziatorino62@gmail.com>
2016-10-04 14:24:59 +0200

    Codice completato con commenti

--- Rubrica_con Gino/src/rubrica/Rubrica.java
+++ Rubrica_con Gino/src/rubrica/Rubrica.java
@@ -7,18 +7,20 @@ import java.util.TreeMap;
 
 public class Rubrica {
 	
-	
+	// ATTRIBUTI
 	private String nome;
 	private Map<String,Voce> voci = new TreeMap<String,Voce>();
 	
-	
+	// COSTRUTTORE VUOTO
 	public Rubrica() {
 	}
 	
+	// COSTRUTTORE CON ATTRIBUTO NOME
 	public Rubrica(String nome) {
 		this.nome = nome;
 	}
 	
+	// METODI GET E SEI
 	public String getNome() {
 		return nome;
 	}
@@ -34,20 +36,45 @@ public class Rubrica {
 	public void setVoci(Map<String, Voce> voci) {
 		this.voci = voci;
 	}
-
-	public Voce getVoce(String nome,String cognome,String telefono)throws VoceGiaEsiste{
+	
+	/*METODO CHE CONTROLLA SE ESISTE LA VOCE PASSATA COME PARAMETRO
+	 *SE ESISTE LANCIA ERRORE
+	 *ALTRIMENTI AGGIUNGE LA NUOVA VOCE E LA RITORNA
+	 */
+	public Voce aggiungiVoce(String nome,String cognome,String telefono)throws VoceGiaEsiste{
 		
 		Voce v = null;
 		
 		if(voci.containsKey(nome+" "+cognome)){
 			throw new VoceGiaEsiste("Voce esistente in Rubrica!");
 		}
+		
 		v = new Voce(nome,cognome,telefono);
 		voci.put(nome+" "+cognome,v);
 		
 		return v;
 	}
 	
+	/*METODO CHE CONTROLLA SE ESISTE LA VOCE PASSATA COME PARAMETRO
+	 *SE NON ESISTA LANCIA ERRORE
+	 *ALTRIMENTI RITORNA LA VOCE
+	 */
+	public Voce getVoce(String nome,String cognome,String telefono)throws VoceNonEsiste{
+		
+		Voce v = null;
+		
+		if(!voci.containsKey(nome+" "+cognome)){
+			throw new VoceNonEsiste("Voce non presente in Rubrica!");
+		}
+		
+		v = voci.get(nome+" "+cognome);
+		return v;
+	}
+	
+	/*METODO CHE CONTROLLA SE LA LISTA E NULLA
+	 * SE E' NULLA LANCIA ECCEZIONE
+	 * ALTRIMENTI RITORNA LA LISTA DELLE VOCI
+	 */
 	public List<Voce> getTuttiVoci() throws VoceNonEsiste{
 		
 		List<Voce> vTemp = null;
@@ -56,23 +83,33 @@ public class Rubrica {
 			throw new VoceNonEsiste("Rubrica vuota!");
 		}
 		
-		vTemp = new ArrayList<Voce>(voci.values());
+		vTemp = new ArrayList<Voce>(voci.values()); // TRASFORMO DA MAPPA VOCI A LISTA VOCI
 		
 		return vTemp;
 	}
 	
-		
+	/*METODO CHE CONTROLLA SE ESISTE LA VOCE PASSATA COME PARAMETRO
+	 * SE NON ESISTE LANCIA ECCEZIONE
+	 * ALTRIMENTI AGGIORNA LA VOCE CON I NUOVI DATI	E LA RITORNA
+	 */
 	public Voce aggiornaVoce(String nome,String cognome,String telefono)throws VoceNonEsiste{
+		
 		Voce v=null;
+		
 		if(!voci.containsKey(nome+" "+cognome)){
 			throw new VoceNonEsiste("Voce non presente");
 		}
+		
 		voci.get(nome+" "+cognome).setTelefono(telefono);
 		v=new Voce(nome,cognome,telefono);
 		
 		return v;
 	}
 	
+	/*METODO CHE CONTROLLA SE ESISTE LA VOCE PASSATA COME PARAMETRO
+	 * SE NON ESISTE LANCIA ECCEZIONE
+	 * ALTRIMENTI CANCELLA LA VOCE E LA RITORNA	
+	 */
 	public Voce cancellaVoce(String nome,String cognome)throws VoceNonEsiste{
 		
 		Voce v = null;
@@ -82,7 +119,6 @@ public class Rubrica {
 		}
 		
 		v = voci.get(nome+" "+cognome);
-		
 		voci.remove(nome+" "+cognome);
 		
 		return v;
--- Rubrica_con Gino/src/rubrica/Voce.java
+++ Rubrica_con Gino/src/rubrica/Voce.java
@@ -2,34 +2,43 @@ package rubrica;
 
 public class Voce {
 	
+	// ATTRIBUTI
 	private String nome;
 	private String cognome;
 	private String telefono;
 	
+	// COSTRUTTORE VUOTO
 	public Voce() {
 	}
 	
+	// COSTRUTTORE CON DATI
 	public Voce(String nome, String cognome, String telefono) {
 		this.nome = nome;
 		this.cognome = cognome;
 		this.telefono = telefono;
 	}
-
+	
+	// METODI GET E SET
 	public String getNome() {
 		return nome;
 	}
+	
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
+	
 	public String getCognome() {
 		return cognome;
 	}
+	
 	public void setCognome(String cognome) {
 		this.cognome = cognome;
 	}
+	
 	public String getTelefono() {
 		return telefono;
 	}
+	
 	public void setTelefono(String telefono) {
 		this.telefono = telefono;
 	}
